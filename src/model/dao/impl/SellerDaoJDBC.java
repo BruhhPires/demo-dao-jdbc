1e1270ed5d59a140e16b94962ccbc2a64a935cfc
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-15 16:54:16 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -53,16 +53,8 @@ public class SellerDaoJDBC implements SellerDao{
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if(rs.next()) { // SE O NEXT NÃO FOR NULO, SERÁ CRIADO O OBJETO
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentID"));
-				dep.setName(rs.getNString("DepName"));
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getNString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep); // DEVE SER INCLUIDO O DEPARTMENT POIS ESSE OBJETO ESTÁ SETADO PELO SELLER
+				Department dep = instantiateDepartment(rs);
+				Seller obj = instantiateSeller(rs, dep);
 				return obj;
 			}
 			return null;
@@ -76,6 +68,24 @@ public class SellerDaoJDBC implements SellerDao{
 			// A CONEXÃO DEVERÁ SER MANTIDA ABERTA
 		}
 
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getNString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep); // DEVE SER INCLUIDO O DEPARTMENT POIS ESSE OBJETO ESTÁ SETADO PELO SELLER
+		return obj;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentID"));
+		dep.setName(rs.getNString("DepName"));
+		return dep;
 	}
 
 	@Override
